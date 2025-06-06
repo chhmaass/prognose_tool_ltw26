cb17b1eb397cb94471f0641e416a28d7e0c3231f
Christoph Maass <ch.h.maass@gmail.com>
2025-06-06 15:44:54 +0200

    Fix false port issue

--- prognose_tool_ltw26.py
+++ prognose_tool_ltw26.py
@@ -339,5 +339,8 @@ def prognose():
     return render_template_string(html_template, result=result_data)
 
 
+# if __name__ == "__main__":
+    # app.run(debug=True, port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
