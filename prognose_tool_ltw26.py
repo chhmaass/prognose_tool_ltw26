115771ee07116c8b1df9c7510617f3e1a82c4629
Christoph Maass <ch.h.maass@gmail.com>
2025-06-06 18:42:15 +0200

    Port adjustment

--- prognose_tool_ltw26.py
+++ prognose_tool_ltw26.py
@@ -332,4 +332,5 @@ def prognose():
     # app.run(debug=True, port=5000)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Port von Railway
+    app.run(debug=True, host="0.0.0.0", port=port)
